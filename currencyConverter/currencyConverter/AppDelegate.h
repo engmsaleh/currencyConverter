31e1a909835e794c5dcb545c0e94c228b254f8dd
Dmitry Volevodz <voley@github.com>
2013-07-25 20:56:59 +0400

    Empty project.

---
+++ currencyConverter/currencyConverter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  currencyConverter
+//
+//  Created by Dmitry on 25.07.13.
+//  Copyright (c) 2013 Dmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ currencyConverter/currencyConverter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  currencyConverter
+//
+//  Created by Dmitry on 25.07.13.
+//  Copyright (c) 2013 Dmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
